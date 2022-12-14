ba1763902bdacfa0f62dca291aca14b06e29b172
zuzannagawrys <72792446+zuzannagawrys@users.noreply.github.com>
2022-12-14 17:37:01 +0100

    Update login.php

--- public/views/login.php
+++ public/views/login.php
@@ -21,7 +21,7 @@
                             }
                         ?>
                     </div>
-                    <input name="email" type="text" placeholder="email@email.com">
+                    <input name="email" type="text" placeholder="em">
                     <input name="password" type="password" placeholder="password">
                     <button type="submit">LOGIN</button>
                 </form>
